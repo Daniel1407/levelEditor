c7023ee5e6eb69728669ed020163496a19ef8a09
Daniel1407 <72376936+Daniel1407@users.noreply.github.com>
2023-03-13 11:57:32 +0100

    level editor terminado

--- levelEditor.py
+++ levelEditor.py
@@ -1,5 +1,6 @@
 import pygame
 import button
+import csv
 
 pygame.init()
 
@@ -18,9 +19,10 @@ pygame.display.set_caption('Level editor')
 
 #variables
 COLS = 16
-MAX_ROWS = 150
+MAX_ROWS = 32
 TILE_SIZE = SCREEN_WIDTH // COLS
 TILE_TYPES = 16
+level = 0
 current_tile = 0
 scroll_up = False
 scroll_down = False
@@ -31,6 +33,9 @@ scroll_speed = 1
 GREEN = (144, 201, 120)
 WHITE = (255, 255, 255)
 RED = (200, 25, 25)
+
+#definir fuente
+font = pygame.font.SysFont('Futura', 20)
 
 #crear una lista de tiles
 world_data = []
@@ -45,18 +50,25 @@ particles = pygame.image.load('img/background/bg1.png').convert_alpha()
 #almacenar tiles en una lista
 img_list = []
 for x in range(TILE_TYPES):
-    img = pygame.image.load(f'img/tile/{x}.png')
+    img = pygame.image.load(f'img/tile/{x}.png').convert_alpha()
     img =  pygame.transform.scale(img, (TILE_SIZE, TILE_SIZE))
     img_list.append(img)
 
+save_img = pygame.image.load('img/save_btn.png').convert_alpha()
+load_img = pygame.image.load('img/load_btn.png').convert_alpha()
+
+#texto en pantalla
+def draw_text(text, font, text_col, x, y):
+    img = font.render(text, True, text_col)
+    screen.blit(img, (x, y))
 
 #dibujar background
 def draw_bg():
     screen.fill(GREEN)
     heigth = background.get_height()
     for x in range(10):
-        screen.blit(background, (0, (-(x * heigth)) + scroll))
-        screen.blit(particles, (0, (-(x * heigth)) + scroll))
+        screen.blit(background, (0, (x * heigth) - scroll))
+        screen.blit(particles, (0, (x * heigth) - scroll))
 
 #dibujar los tiles
 def draw_world():
@@ -67,8 +79,6 @@ def draw_world():
 
 #dibujar cuadricula
 def draw_grid():
-    heigth = background.get_height()
-    width = background.get_width()
     #lineas verticales
     for c in range(COLS + 1):
         pygame.draw.line(screen, WHITE, (c * TILE_SIZE, 0), (c * TILE_SIZE, SCREEN_HEIGTH))
@@ -78,6 +88,8 @@ def draw_grid():
         pygame.draw.line(screen, WHITE, (0, c * TILE_SIZE - scroll), (SCREEN_WIDTH, c * TILE_SIZE - scroll))
 
 #crear botones
+save_button = button.Button(SCREEN_WIDTH + SIDE_MARGIN - 250, SCREEN_HEIGTH - 75, save_img, 1)
+load_button = button.Button(SCREEN_WIDTH + SIDE_MARGIN - 125, SCREEN_HEIGTH - 75, load_img, 1)
 button_list = []
 button_col = 0
 button_row = 0
@@ -90,7 +102,6 @@ for i in range(len(img_list)):
         button_col = 0
 
 run = True
-
 while run:
 
     clock.tick(FPS)
@@ -98,7 +109,26 @@ while run:
     draw_bg()
     draw_grid()
     draw_world()
-    
+    draw_text(f'Level: {level}', font, WHITE, SCREEN_WIDTH + SIDE_MARGIN - 170, SCREEN_HEIGTH - 140)
+    draw_text('RKEY o LKEY para cambiar nivel', font, WHITE, SCREEN_WIDTH + SIDE_MARGIN - 250, SCREEN_HEIGTH - 110)
+
+    #save y load
+    if save_button.draw(screen):
+        #guardar nivel
+        with open(f'level_data{level}.csv', 'w', newline='') as csvfile:
+            write = csv.writer(csvfile, delimiter = ',')
+            for row in world_data:
+                write.writerow(row)
+    if load_button.draw(screen):
+        #cargar nivel
+        #mover el scroll al inicio del nivel
+        scroll = 0
+        with open(f'level_data{level}.csv', newline='') as csvfile:
+            reader = csv.reader(csvfile, delimiter = ',')
+            for x, row in enumerate(reader):
+                for y, tile in enumerate(row):
+                    world_data[x][y] = int(tile)
+                
     #escoger un tile
     button_count = 0
     for button_count, i in enumerate(button_list):
@@ -109,10 +139,10 @@ while run:
     pygame.draw.rect(screen, RED, button_list[current_tile].rect, 2)
 
     #scroll
-    if scroll_up == True:
-        scroll += 3 * scroll_speed
-    if scroll_down == True and scroll > 0:
+    if scroll_up == True and scroll > 0:
         scroll -= 3 * scroll_speed
+    if scroll_down == True and scroll < (MAX_ROWS * TILE_SIZE) - SCREEN_HEIGTH:
+        scroll += 3 * scroll_speed
 
     pos = pygame.mouse.get_pos()
     x = pos[0] // TILE_SIZE
@@ -121,8 +151,8 @@ while run:
     if pos[0] < SCREEN_WIDTH and pos[1] < SCREEN_HEIGTH:
 		#update tile value
         if pygame.mouse.get_pressed()[0] == 1:
-                    if world_data[y][x] != current_tile:
-                        world_data[y][x] = current_tile         
+            if world_data[y][x] != current_tile:
+                world_data[y][x] = current_tile         
         if pygame.mouse.get_pressed()[2] == 1:
             world_data[y][x] = -1
 
@@ -131,6 +161,10 @@ while run:
             run = False
         #teclas pulsadas
         if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                level += 1
+            if event.key == pygame.K_LEFT and level > 0:
+                level -= 1
             if event.key == pygame.K_UP:
                 scroll_up = True
             if event.key == pygame.K_DOWN:
@@ -144,8 +178,6 @@ while run:
                 scroll_down = False
             if event.key == pygame.K_LSHIFT:
                 scroll_speed = 1
-
-            
 
     pygame.display.update()
 
